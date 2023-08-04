1e23c0c639d64e64e23f0839704bb79880f3c5f4
kconte18 <kconte810@gmail.com>
2023-08-04 15:53:35 -0500

    Implemented average rankings

--- src/app/features/rankings-table/rankings-table.component.ts
+++ src/app/features/rankings-table/rankings-table.component.ts
@@ -12,6 +12,7 @@ export class RankingsTableComponent implements OnChanges, OnInit {
   @Input() positionType!: string;
 
   allRankings: Ranking[] = [];
+  averageRankings: Ranking[] = [];
   chunk: Ranking[] = [];
 
   constructor(private rankingsService: RankingsService) { }
@@ -36,9 +37,37 @@ export class RankingsTableComponent implements OnChanges, OnInit {
   callGetRankingsService(): void {
     this.rankingsService.getRankings(this.scoreType, this.positionType).subscribe(response => {
       this.allRankings = response;
-      this.allRankings.sort(function (a, b) {
-        return a.rank - b.rank || a.ranking_src.ranking_src_name.charCodeAt(0) - b.ranking_src.ranking_src_name.charCodeAt(0) || a.ranking_src.ranking_src_name.charCodeAt(1) - b.ranking_src.ranking_src_name.charCodeAt(1)
+      this.averageRankings = this.allRankings.filter((ranking: Ranking) => {
+        return ranking.ranking_src.ranking_src_name === 'AVERAGE';
       });
+      this.allRankings = this.allRankings.filter((ranking: Ranking) => {
+        return ranking.ranking_src.ranking_src_name !== 'AVERAGE';
+      });
+      this.allRankings.sort(function (a, b) {
+        return a.rank - b.rank || a.ranking_src.ranking_src_name.charCodeAt(0) - b.ranking_src.ranking_src_name.charCodeAt(0) || a.ranking_src.ranking_src_name.charCodeAt(1) - b.ranking_src.ranking_src_name.charCodeAt(1);
+      });
+      this.averageRankings.sort(function (a, b) {
+        return a.rank - b.rank;
+      });
+      this.sliceAverageRankingsByPosition();
     });
+  }
+
+  sliceAverageRankingsByPosition(): void {
+    if(this.positionType == 'overall') {
+      this.averageRankings = this.averageRankings.slice(0,100)
+    }
+    else if(this.positionType == 'qb') {
+      this.averageRankings = this.averageRankings.slice(0,32)
+    }
+    else if(this.positionType == 'rb'){
+      this.averageRankings = this.averageRankings.slice(0,50)
+    }
+    else if(this.positionType == 'wr'){
+      this.averageRankings = this.averageRankings.slice(0,50)
+    }
+    else if(this.positionType == 'te'){
+      this.averageRankings = this.averageRankings.slice(0,40)
+    }
   }
 }
