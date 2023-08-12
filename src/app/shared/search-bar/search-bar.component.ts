7aced663b2ae5514ae6c3a3cc14cdf671957e8c0
kconte18 <kconte810@gmail.com>
2023-08-11 22:25:50 -0500

    Added animated search icon

--- src/app/shared/search-bar/search-bar.component.ts
+++ src/app/shared/search-bar/search-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, HostListener, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-search-bar',
@@ -8,14 +8,30 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 export class SearchBarComponent implements OnInit {
   @Output() textEvent= new EventEmitter<string>(); 
 
+  focus: boolean = false;
+
   inputText: string = '';
   timerToSend: any;
 
-  constructor() { }
+  constructor(private eRef: ElementRef) { }
 
   ngOnInit(): void {
   }
+
+  @HostListener('document:click', ['$event'])
+  clickout(event: any) {
+    if(!(this.eRef.nativeElement.contains(event.target)) && this.focus == true) {
+      this.toggleFocus();
+    }
+    if((this.eRef.nativeElement.contains(event.target)) && this.focus == false) {
+      this.toggleFocus();
+    }
+  }
   
+  toggleFocus(): void {
+    this.focus = !this.focus;
+  }
+
   sendInputText(): void {
     // clearTimeout(this.timerToSend)
     // this.startTimerToSend();
